dd0f4329d90bfdbdc4ef19e3f99ffa9cc73e47de
levonpetrosyan93 <petrosyan.levon93@gmail.com>
2023-11-24 05:00:06 +0400

    Revert temperarry links

--- scripts/install-insight.sh
+++ scripts/install-insight.sh
@@ -17,9 +17,8 @@ $lib_name/bin/bitcore-node-zcoin create $node_name
 
 cd $node_name
 
-npm i firoorg/bitcore-node-zcoin#spark
 node_modules/bitcore-node-zcoin/bin/bitcore-node-zcoin install firoorg/insight-ui-zcoin
-node_modules/bitcore-node-zcoin/bin/bitcore-node-zcoin install firoorg/insight-api-zcoin#spark
+node_modules/bitcore-node-zcoin/bin/bitcore-node-zcoin install firoorg/insight-api-zcoin
 
 find . -name "zcore-lib" -type d -not -path "./node_modules/zcore-lib" | xargs rm -rf
 
